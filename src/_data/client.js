c36b3ef2c5d7cfa2b3a6a23bc00182b739debbbf
IqraYasir <iqrayasir0906@gmail.com>
2024-12-31 22:57:53 +0000

    Add in home page components

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,15 +1,12 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "Gym Tiger Fitness Centre",
+    email: "customer@gymtiger.com",
+    phoneForTel: "555-779-4408",
+    phoneFormatted: "(555) 779-448",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
-        country: "US",
+        city: "Liverpool",
+        zip: "LQ4 5DT",
+        country: "UK",
         mapLink: "https://goo.gl/maps/UAQn4vuGDiwv7DV39",
     },
     socials: {
